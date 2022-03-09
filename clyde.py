96a89f97f93eb32bb832eedb99cb8f0f8fc34ae9
Tanasia649 <schoolkiddo25@gmail.com>
2022-03-08 21:49:10 -0800

    changed

--- clyde.py
+++ clyde.py
@@ -1,4 +1,5 @@
-import hikari
+import lightbulb
 
-bot = hikari.GatewayBot(token="OTUwMjM4MzM5NjQxMDE2MzUw.YiWAag.uG8UqlVKqy8z2gAJWM4KEgdxf7E")
+bot = lightbulb.BotApp(token="OTUwMjM4MzM5NjQxMDE2MzUw.YiWAag.uG8UqlVKqy8z2gAJWM4KEgdxf7E")
+
 bot.run()
