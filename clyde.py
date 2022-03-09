7103fedc54ee9bed6aa40051469b38a2bdc9c6c2
Tanasia649 <schoolkiddo25@gmail.com>
2022-03-09 14:05:24 -0800

    files

--- clyde.py
+++ clyde.py
@@ -1,5 +1,22 @@
 import lightbulb
+import hikari
 
-bot = lightbulb.BotApp(token="OTUwMjM4MzM5NjQxMDE2MzUw.YiWAag.uG8UqlVKqy8z2gAJWM4KEgdxf7E")
+bot = lightbulb.BotApp(
+    token="OTUwMjM4MzM5NjQxMDE2MzUw.YiWAag.MCMlsJyFyAWsvMveg2uB_9pY6dY",
+    default_enabled_guilds=933809192215654400
+)
+
+
+@bot.listen(hikari.StartedEvent)
+async def on_started(event):
+    print("Clyde is up and running")
+
+
+@bot.command
+@lightbulb.command('ping', 'says pong')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong')
+
 
 bot.run()
